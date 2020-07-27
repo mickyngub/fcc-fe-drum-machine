de813b4fe1b6d741700704f597aee634a5dba28e
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-27 22:09:15 +0700

    Last test left

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import "./App.css";
 const App = () => {
   return (
     <div id="drum-machine" className="App" style={{ margin: 30 }}>
-      Hello React!
       <DrumGrid></DrumGrid>
-      <div id="Heater-1" className="drum-pad">
-        <audio
-          className="clip"
-          id="Q"
-          src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
-        ></audio>
-        testing
-      </div>
     </div>
   );
 };
--- src/Components/DrumGrid.js
+++ src/Components/DrumGrid.js
@@ -1,7 +1,59 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect, useCallback } from "react";
 import Grid from "@material-ui/core/Grid";
 
 const DrumGrid = () => {
+  const [text, setText] = useState("");
+  const keyPressHandler = useCallback((event) => {
+    let { key } = event;
+    key = key.toUpperCase();
+    console.log(key);
+    switch (key) {
+      case "Q":
+        setText("Heater-1");
+        playSoundQ();
+        break;
+      case "W":
+        setText("Heater-2");
+        playSoundW();
+        break;
+      case "E":
+        setText("Heater-3");
+        playSoundE();
+        break;
+      case "A":
+        setText("Heater-4");
+        playSoundA();
+        break;
+      case "S":
+        setText("Open-HH");
+        playSoundS();
+        break;
+      case "D":
+        setText("Kick n hat");
+        playSoundD();
+        break;
+      case "Z":
+        setText("Closed HH");
+        playSoundZ();
+        break;
+      case "X":
+        setText("Snare");
+        playSoundX();
+        break;
+      case "C":
+        setText("Clap");
+        playSoundC();
+        break;
+      default:
+        break;
+    }
+  }, []);
+  useEffect(() => {
+    document.addEventListener("keydown", keyPressHandler);
+    return () => {
+      document.removeEventListener("keydown", keyPressHandler);
+    };
+  }, [keyPressHandler]);
   let audioQ = useRef();
   let audioW = useRef();
   let audioE = useRef();
@@ -50,12 +102,13 @@ const DrumGrid = () => {
   };
   return (
     <div id="display">
+      {text && text}
       <Grid container spacing={3} style={{ marginTop: 20 }}>
         <Grid
           item
+          xs
           id="Heater1"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "steelblue", margin: 10 }}
           onClick={playSoundQ}
         >
@@ -70,9 +123,9 @@ const DrumGrid = () => {
         </Grid>
         <Grid
           item
+          xs
           id="Heater2"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "steelblue", margin: 10 }}
           onClick={playSoundW}
         >
@@ -86,9 +139,9 @@ const DrumGrid = () => {
         </Grid>
         <Grid
           item
+          xs
           id="Heater3"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "steelblue", margin: 10 }}
           onClick={playSoundE}
         >
@@ -104,9 +157,9 @@ const DrumGrid = () => {
       <Grid container spacing={3}>
         <Grid
           item
+          xs
           id="Heater4"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "pink", margin: 10 }}
           onClick={playSoundA}
         >
@@ -120,9 +173,9 @@ const DrumGrid = () => {
         </Grid>
         <Grid
           item
+          xs
           id="Clap"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "pink", margin: 10 }}
           onClick={playSoundS}
         >
@@ -136,9 +189,9 @@ const DrumGrid = () => {
         </Grid>
         <Grid
           item
+          xs
           id="Open-HH"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "pink", margin: 10 }}
           onClick={playSoundD}
         >
@@ -154,9 +207,9 @@ const DrumGrid = () => {
       <Grid container spacing={3}>
         <Grid
           item
+          xs
           id="Kick-n'-Hat"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "yellow", margin: 10 }}
           onClick={playSoundZ}
         >
@@ -170,9 +223,9 @@ const DrumGrid = () => {
         </Grid>
         <Grid
           item
+          xs
           id="Kick"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "yellow", margin: 10 }}
           onClick={playSoundX}
         >
@@ -186,9 +239,9 @@ const DrumGrid = () => {
         </Grid>
         <Grid
           item
+          xs
           id="Closed-HH"
           className="drum-pad"
-          xs
           style={{ backgroundColor: "yellow", margin: 10 }}
           onClick={playSoundC}
         >
@@ -201,6 +254,7 @@ const DrumGrid = () => {
           C
         </Grid>
       </Grid>
+      {text && <h1>{text}</h1>}
     </div>
   );
 };
