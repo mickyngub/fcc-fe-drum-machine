27835c7cfc34d54decfe5a530d652cc4d2db33db
mickyngub <57980116+mickyngub@users.noreply.github.com>
2020-07-27 13:14:35 +0700

    Add style for grid

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import DrumGrid from "./Components/DrumGrid";
 import "./App.css";
 
-function App() {
+const App = () => {
+  let a = ["a", "b", 4];
+  let b = { a: 4, b: "dsdf" };
+
+  console.log(a);
+  console.table(a);
+  console.log(b);
+  console.table(b);
   return (
-    <div className="App">
+    <div className="App" style={{ margin: 30 }}>
       Hello React!
       <DrumGrid></DrumGrid>
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/DrumGrid.js
+++ src/Components/DrumGrid.js
@@ -4,37 +4,36 @@ import Grid from "@material-ui/core/Grid";
 const DrumGrid = () => {
   return (
     <div>
-      Hello
-      <Grid container spacing={3}>
-        <Grid item xs>
+      <Grid container spacing={3} style={{ marginTop: 20 }}>
+        <Grid item xs style={{ backgroundColor: "steelblue", margin: 10 }}>
           yo
         </Grid>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "steelblue", margin: 10 }}>
           yo
         </Grid>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "steelblue", margin: 10 }}>
           yo
         </Grid>
       </Grid>
       <Grid container spacing={3}>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "pink", margin: 10 }}>
           sup
         </Grid>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "pink", margin: 10 }}>
           sup
         </Grid>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "pink", margin: 10 }}>
           sup
         </Grid>
       </Grid>
       <Grid container spacing={3}>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "yellow", margin: 10 }}>
           hey
         </Grid>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "yellow", margin: 10 }}>
           hey
         </Grid>
-        <Grid item xs>
+        <Grid item xs style={{ backgroundColor: "yellow", margin: 10 }}>
           hey
         </Grid>
       </Grid>
